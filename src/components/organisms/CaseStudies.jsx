6aa0c82e05edb4d4b96737d8f30f71eff3aebaba
AlejoSuarez2001 <alejo.2001.suarez@gmail.com>
2026-01-06 12:52:20 -0300

    fix

--- src/components/organisms/CaseStudies.jsx
+++ src/components/organisms/CaseStudies.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Container, Stack } from '@chakra-ui/react';
+import { Box, Stack } from '@chakra-ui/react';
 import Title from '../molecules/Title';
 import { useTranslation } from 'react-i18next';
 import CaseStudyItem from '../molecules/CaseStudyItem';
